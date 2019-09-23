7434d70aea63449e5b2c9ceb3ab711a233ccc632
Vijay Sai Jampani <jampanivijaysai2@gmail.com>
2019-09-23 13:25:30 -0400

    Website
    website

---
+++ williamspdr/jquery.js
@@ -0,0 +1,9 @@
+$('.card').hover(
+       function(){ $(this).addClass('shadow-lg') },
+       function(){ $(this).removeClass('shadow-lg') }
+)
+
+// $('.card').not(.hover)(
+//        function(){ $(this).addClass('shadow') },
+//        function(){ $(this).removeClass('shadow-lg') }
+// )
